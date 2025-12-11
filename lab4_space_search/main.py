6820d25708c2d12302745ab147290330da62ce16
Viktoriia Nowotka <98872709+Victoriia-Ripka@users.noreply.github.com>
2025-12-11 22:18:34 +0100

    dodano raport

--- lab4_space_search/main.py
+++ lab4_space_search/main.py
@@ -5,13 +5,12 @@ from charts import *
 
 def main():
     r_min, r_max = -32.768, 32.768
-    max_iter = 200
+    max_iter = 300
     # TODO zbadać wpływ rozmiaru kroku na działanie algorytmu
-    steps = [0.01, 0.02, 0.05, 0.075, 0.1] # 0.005, 0.01, 0.015, 0.02, 0.05, 0.075, 0.09, 0.1, 0.15, 0.2, 0.25
+    steps = [0.05, 0.075, 0.1, 0.15, 0.02, 0.5] # 0.005, 0.01, 0.015, 0.02, 0.05, 0.075, 0.09, 0.1, 0.15, 0.2, 0.25
     # TODO zbadać wpływ punktu początkowego
     x0 = 23.0
     y0 = 0.0
-
     history_1d = {}
     final_values_1d = []
 
@@ -34,12 +33,29 @@ def main():
     # 1D Wykres trajektoria minimalizacji z najlepszym krokiem
     best_index = np.argmin(final_values_1d)
     optimal_step = steps[best_index]
-    gradient_descent = GradientDescent(r_min, r_max, optimal_step, max_iter)
-    final_pos, history_f, path_x, path_y = gradient_descent.solve_with_trajectory(ackley_function, x0, y0)
+    gd = GradientDescent(r_min, r_max, optimal_step, max_iter)
+    final_pos, history_f, path_x, path_y = gd.solve_with_trajectory(ackley_function, x0, y0)
     visualize_best_step_2d(final_pos, history_f, path_x, optimal_step)
 
+    # 1D Wykres wpływu punktu początkowego
+    x_array = [1, 5, 10, 20, 27]
+    iterations_list = []
+
+    for x0 in x_array:
+        gd.stop_iter = None
+        gd.solve(ackley_function, x0, 0.0)
+        iterations_list.append(gd.stop_iter)
+
+    plt.figure(figsize=(12, 7))
+    plt.plot(x_array, iterations_list, marker='o', linewidth=2)
+    plt.title("Wykres dodatkowy. Wpływ punktu początkowego na liczbę iteracji (Ackley 1D)")
+    plt.xlabel("Punkt początkowy")
+    plt.ylabel("Liczba iteracji")
+    plt.grid(True)
+    plt.show()
+
     # TODO zbadać wpływ rozmiaru kroku na działanie algorytmu
-    steps = [0.15, 0.2, 0.25, 0.3, 0.35]
+    steps = [0.05, 0.075, 0.1, 0.15, 0.2, 0.35, 0.5]
     # TODO zbadać wpływ punktu początkowego
     x0 = 10.0
     y0 = 10.0
@@ -65,9 +81,26 @@ def main():
     # 2D Wykres trajektoria minimalizacji z najlepszym krokiem
     best_index = np.argmin(final_values_2d)
     optimal_step = steps[best_index]
-    gradient_descent = GradientDescent(r_min, r_max, optimal_step, max_iter)
-    final_pos, history_f, path_x, path_y = gradient_descent.solve_with_trajectory(ackley_function_2d, x0, y0)
+    gd = GradientDescent(r_min, r_max, optimal_step, max_iter)
+    final_pos, history_f, path_x, path_y = gd.solve_with_trajectory(ackley_function_2d, x0, y0)
     visualize_best_step_3d(final_pos, history_f, path_x, path_y, optimal_step)
+
+    # 1D Wykres wpływu punktu początkowego
+    x_array = [1, 5, 10, 20, 27]
+    iterations_list = []
+
+    for i in  enumerate(x_array) :
+        gd.stop_iter = None
+        gd.solve(ackley_function, i[0], i[1])
+        iterations_list.append(gd.stop_iter)
+
+    plt.figure(figsize=(12, 7))
+    plt.plot(x_array, iterations_list, marker='o', linewidth=2)
+    plt.title("Wykres dodatkowy. Wpływ punktu początkowego na liczbę iteracji (Ackley 2D)")
+    plt.xlabel("Punkt początkowy")
+    plt.ylabel("Liczba iteracji")
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == "__main__":
