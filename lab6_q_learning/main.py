86baca588da33017e9f1524b69bcdc281f361354
Viktoriia Nowotka <98872709+Victoriia-Ripka@users.noreply.github.com>
2026-01-18 20:31:36 +0100

    q_learn algorithm

--- lab6_q_learning/main.py
+++ lab6_q_learning/main.py
@@ -5,19 +5,29 @@ from lab6_q_learning.q_learn import QLearning
 
 
 def main():
-    t_max = 1
-    e_max = 1
     U = [0, 1, 2]
     n_dirs = 4
 
-    # Wykonaj przy tym eksperymenty dla różnych zestawów parametrów, uwzględniając:
-    eps = 0.01              # parametr eksploracji ϵ
-    b = 0.1                 # szybkość uczenia β
-    y = 0.9                 # współczynnik dyskontowania γ
+    e_max = 15
+    max_steps = 100
 
-    ql = QLearning(U, n_dirs, e_max, t_max)
+    # Wykonaj przy tym eksperymenty dla różnych zestawów parametrów, uwzględniając:
+    b = 0.25                 # szybkość uczenia β
+    y = 0.95                 # współczynnik dyskontowania γ
+    eps = 0.25               # parametr eksploracji ϵ
+
+    ql = QLearning(U, n_dirs, e_max, max_steps)
     ql.train(b, y, eps)
+    n_steps = ql.run()
     ql.close()
+
+# współczynnik dyskontowania γ = 1 - 1/H, gdzie H = horyzont kroków.
+# Przy H = 10 => γ = 0.9
+# Przy H = 20 => γ = 0.95
+# Przy H = 40 => γ = 0.975
+# Przy H = 100 => γ = 0.99
+
+#  parametr eksploracji ϵ
 
 if __name__ == "__main__":
     main()
--- lab6_q_learning/q_learn.py
+++ lab6_q_learning/q_learn.py
@@ -7,87 +7,136 @@ import numpy as np
 
 # Q-learning z epizodami oraz ϵ-zachłanną strategią wyboru akcji
 # * Dostosuj rozwiązanie, aby działało również dla losowych pozycji agenta i celu.
-# Terminations:
-# The agent reaches the goal.
-# Timeout (see max_steps).
 
 class QLearning:
-    def __init__(self, actions, n_dirs, e_max, t_max):
+    def __init__(self, actions, n_dirs, e_max, max_steps=100, goal_pos=(2, 2), seed=42):
+        self.Q = np.array([])
+        self.seed = seed
         self.e_max = e_max
-        self.t_max = t_max
 
-        self.env = gym.make("MiniGrid-FourRooms-v0", max_steps=10, goal_pos=(2, 2), agent_pos=(7, 7), render_mode="human")
+        self.env = gym.make("MiniGrid-FourRooms-v0", max_steps=max_steps, goal_pos=goal_pos, render_mode="human")
+        self.base_env = self.env.unwrapped
+
+        self.U = actions                    # actions: left, right, forward
+        self.n_dirs = n_dirs                # 4 directions
+        self.X = self.__init_states()       # available states: (x, y, dir)
+
         self.state_to_id = self.__build_state_mapping()
 
-        self.U = actions            # actions: left, right, forward
-        self.n_dirs = n_dirs        # 4 directions
-        self.__init_states()        # available states: (x, y, dir)
+    def __valid_positions(self):
+        valid = []
 
-    # TODO REFACTOR
-    def __init_states(self):
-        self.X = []
-
-        base_env = self.env.unwrapped
-        for x in range(base_env.width):
-            for y in range(base_env.height):
-                cell = base_env.grid.get(x, y)
+        for x in range(self.base_env.width):
+            for y in range(self.base_env.height):
+                cell = self.base_env.grid.get(x, y)
                 if cell is None or cell.can_overlap():
-                    self.X.append((x, y))
+                    valid.append((x, y))
+        return valid
+
+    def __init_states(self):
+        return self.__valid_positions()
 
     def __build_state_mapping(self):
-        base_env = self.env.unwrapped
+        # state_to_id: (x, y, dir) -> index
         state_to_id = {}
         idx = 0
 
-        for x in range(base_env.width):
-            for y in range(base_env.height):
-                cell = base_env.grid.get(x, y)
-                if cell is None or cell.can_overlap():
-                    for d in range(4):
-                        state_to_id[(x, y, d)] = idx
-                        idx += 1
+        for (x, y) in self.__valid_positions():
+            for d in range(4):
+                state_to_id[(x, y, d)] = idx
+                idx += 1
 
         return state_to_id
 
     def __define_x(self):
-        base_env = self.env.unwrapped
-        x, y = base_env.agent_pos
-        d = base_env.agent_dir
+        x, y = self.base_env.agent_pos
+        d = self.base_env.agent_dir
+        return (x, y, d), self.state_to_id[(x, y, d)]
 
-        return self.state_to_id[(x, y, d)]
+    def __set_random_agent_pos(self):
+        valid = self.__valid_positions()
+        pos = valid[np.random.randint(len(valid))]
+        self.base_env.agent_pos = np.array(pos)
+        self.base_env.agent_dir = np.random.randint(4)
 
-    def choose_action(self, x, Q, eps):
+    @staticmethod
+    def print_agent_position(pos):
+        x, y, dir = pos
+        print(f"Agent grid pos: {x}; {y} | dir: {dir}")
+
+    @staticmethod
+    def choose_action(x, Q, eps):
         if np.random.random() < eps:
-            return np.random.choice(self.U)     # eksploracja
-        else:
-            return np.argmax(Q[x])              # eksploatacja
+            return np.random.choice(Q.shape[1])  # eksploracja: losowa akcja
+
+        q_values = Q[x]
+        max_q = np.max(q_values)
+        best_actions = np.where(q_values == max_q)[0]
+
+        return np.random.choice(best_actions)    # eksploatacja: najlepsze
+
+    @staticmethod
+    def log(e, t):
+        if t % 100 == 0:
+            print(f"Episode: {e+1}, step: {t}")
 
     def train(self, b, y, eps=0.1):
         e = 0
         Q = np.zeros((len(self.X) * self.n_dirs, len(self.U)))
 
         while e < self.e_max:
-
+            self.env.reset(seed=self.seed)
             t = 0
-            obs, info = self.env.reset(seed=42)
-            x = self.__define_x()
+            terminated = False
+            truncated = False
 
-            while t < self.t_max:
+            # kady epizod ma rózny punkt A
+            self.__set_random_agent_pos()
+            pos_x, x = self.__define_x()
+            self.print_agent_position(pos_x)
+
+            while not terminated or not truncated:
+                self.log(e, t)
+                x_prev = x
+
                 act = self.choose_action(x, Q, eps)
-                obs, rew, terminated, truncated, info = self.env.step(act)
-                x = self.__define_x()
+                _, rew, terminated, truncated, _ = self.env.step(act)
+                _, x = self.__define_x()
 
-                delta = rew + y * max(Q[x, act]) - Q[x, act]
-                Q[x, act] += b * delta
+                delta = rew + y * max(Q[x]) - Q[x_prev][act]
+                Q[x][act] += b * delta
+
                 t += 1
 
-                if terminated or truncated:
-                    x, info = self.env.reset()
-            e += 1
-        self.env.reset()
+                if truncated:
+                    print("STOP: timeout")
+                    self.env.reset(seed=self.seed)
 
-    def run(self):
-        pass
+                if terminated:
+                    print("STOP: reached goal")
+                    self.env.reset(seed=self.seed)
+            e += 1
+
+        self.Q = Q
+
+    def run(self, agent_pos=(7,7)):
+        self.env.reset(seed=self.seed)
+        x = self.__define_x()
+
+        terminated = False
+        truncated = False
+        n_steps = 0
+        eps = 0
+
+        while not terminated and not truncated:
+            action = self.choose_action(x, self.Q, eps)
+            _, _, terminated, truncated, _ = self.env.step(action)
+
+            x = self.__define_x()
+            n_steps += 1
+
+        print(f"Run finished after {n_steps} steps")
+        return n_steps
 
     def close(self):
         self.env.close()
