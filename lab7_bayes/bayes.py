3226befd789544f1804e3d5cd06b0b3bda65508c
Viktoriia Nowotka <98872709+Victoriia-Ripka@users.noreply.github.com>
2026-01-20 19:15:51 +0100

    lab 7 started

---
+++ lab7_bayes/bayes.py
@@ -0,0 +1,26 @@
+"""
+Author: Viktoriia Nowotka
+
+Celem ćwiczenia jest implementacja algorytmu naiwnego klasyfikatora Bayesa.
+Następnie należy wykorzystać stworzony algorytm do stworzenia i zbadania jakości
+klasyfikatorów dla zbioru danych SMS Spam Collection Dataset
+https://www.kaggle.com/datasets/uciml/sms-spam-collection-dataset.
+Klasą jest pole class.
+"""
+
+class Bayes:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def predict_proba(self, X):
+        pass
+
+    def score(self, X, y):
+        pass
+
---
+++ lab7_bayes/main.py
@@ -0,0 +1,13 @@
+"""
+Author: Viktoriia Nowotka
+"""
+import numpy as np
+import pandas as pd
+# import scikit-learn
+
+def main():o
+    pass
+
+
+if __name__ == "__main__":
+    main()
